ded32a14889ba38abaf24fb6f982df3268480190
TenaarFeiri <tenaarfeiri@gmail.com>
2026-06-21 06:46:09 +0200

    Update app.lua

--- app.lua
+++ app.lua
@@ -1,23 +1,5 @@
 ngx.header.content_type = "text/plain"
 
---[[
-local function custom_loader(module_name)
-    local path = module_name:gsub("%.", "/") .. ".lua"
-    local file = io.open("src/" .. path, "r")
-    
-    if file then
-        local content = file:read("*a")
-        file:close()
-        return loadstring(content, "@" .. path)
-    end
-    
-    return nil, "module '" .. module_name .. "' not found"
-end
- 
-table.insert(package.loaders or package.searchers, 2, custom_loader)
- 
-package.path = package.path .. ";src/?.lua"]]
-
 -- Hold globals, like important strings
 -- Stuff we're fine with exposing.
 _G.GLOBALS = {
